0f02713eded6209c52b452b5e1e8964879150935
unknown <1danabana@gmail.com>
2019-07-05 18:26:53 -0700

    trying to get keyboard event to work for alphabet

--- game.js
+++ game.js
@@ -1,7 +1,30 @@
-var Wins;
+//array of letters for choosing from
+var alphabet = ['l','m','n','t','u','v','w','x','y','z','c','d','o','a','b','p','q','r','s','e','f','g','h','i','j','k',];
+console.log(alphabet);
+// counts the amount of times the player has guessed correctly 
+var Wins = 0;
 
-var Losses;
+// counts how many times the player has guessed wrongly
+var Losses = 0;
 
-var Guesses_left;
+// lets the player know how many more guesses they have before game over
+var Guesses_left = 9;
 
-var Guesses_so_far;
+// tells the player what they've guessed so far
+var Guesses_so_far = 0;
+
+//creating function based off of activity 34 day-3 week-3
+function renderLetter(){
+  if (Wins <= (alphabet.length - 1)){
+    document.querySelector('#Wins').innerHTML = alphabet[Wins];
+  }
+  else {
+    document.querySelector('#Wins').innerHTML = "game";
+  }
+}
+//keypress events 
+var playerinput = event.key();
+console.log (playerinput);
+if (playerinput === 'a'){
+  alert('l');
+}
